2f0e7a9e88ade94e2670720c69e62a78fa11ff4a
Eric <spb828@naver.com>
2018-03-02 11:15:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import json
 from subprocess import call
-call(["python", "split.py", "-yt", "https://www.youtube.com/watch?v=-0DLPzsiXXE"])
+from pprint import pprint
+
+ontologyData = json.load(open('ontology.json'))
+trainedData = json.load(open('balanced_train_segments.json'))
+
+def ontologyFindByID(id):
+    for ontology in ontologyData:
+        if ontology['id'] == id:
+            return ontology
+
+#call(["python", "split.py", "-yt", "https://www.youtube.com/watch?v=-0DLPzsiXXE"])
+found = ontologyFindByID("/m/07qwdck")
+pprint(found)
+
+# create destination folder
+if not os.path.exists(FOLDER) and not DRYRUN:
+    os.makedirs(FOLDER)
