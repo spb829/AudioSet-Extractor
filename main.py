2b434c9e042b62c65aa2941550291fa6df1cec59
Eric Park <spb828@naver.com>
2018-10-09 02:50:38 +0900

    Update
    FIx encoding error
    Add function ontologyFindByLabel
    Add mode download by using id and label name

--- main.py
+++ main.py
@@ -2,51 +2,110 @@ import json
 import os
 from subprocess import call
 from shutil import copyfile
+import argparse
 
-ontologyData = json.load(open('ontology.json'))
-trainedData = json.load(open('balanced_train_segments.json'))
+ontologyData = json.load(open('ontology.json', 'rb'))
+trainedData = json.load(open('balanced_train_segments.json', 'rb'))
 resultFolder = "./results/"
 
-def ontologyFindByID(id):
+def ontologyFindById(id):
     for ontology in ontologyData:
         if ontology['id'] == id:
             return ontology
 
-for data in trainedData:
-    src = "./splits/" + data['YTID'] + "/01 - " + data['YTID'] + ".mp3"
-    if os.path.exists(src):
-        continue
+def ontologyFindByLabel(label):
+    for ontology in ontologyData:
+        if ontology['name'] == label:
+            return ontology
 
-    url = "https://www.youtube.com/watch?v=" + data['YTID']
-    f = open("tracks.txt", 'w')
+def download(mode, value):
+  for data in trainedData:
+      if mode is 'id':
+          labelIds = data['positive_labels'].split(',')
+          flag = False
+          for labelId in labelIds:
+                if labelId == value:
+                    flag = True
+                    break
+          if flag is False:
+              continue
+      elif mode is 'label':
+          labelIds = data['positive_labels'].split(',')
+          for labelId in labelIds:
+              found = ontologyFindById(value)
+              if found['name'] == value:
+                  flag = True
+                  break
+          if flag is False:
+              continue
 
-    startSeconds = data['start_seconds']
-    min = int(startSeconds / 60)
-    seconds = int(startSeconds % 60)
-    line = str(min) + ":" + str(seconds) + " " + data['YTID'] + "\n"
-    f.write(line)
+      src = "./splits/" + data['YTID'] + "/01 - " + data['YTID'] + ".mp3"
+      if os.path.exists(src):
+          continue
 
-    startSeconds += 10
-    min = int(startSeconds / 60)
-    seconds = int(startSeconds % 60)
-    line = str(min) + ":" + str(seconds) + " sample1"
-    f.write(line)
+      url = "https://www.youtube.com/watch?v=" + data['YTID']
+      f = open("tracks.txt", 'w')
 
-    f.close()
+      startSeconds = data['start_seconds']
+      min = int(startSeconds / 60)
+      seconds = int(startSeconds % 60)
+      line = str(min) + ":" + str(seconds) + " " + data['YTID'] + "\n"
+      f.write(line)
 
-    call(["python", "split.py", "-yt", url])
+      startSeconds += 10
+      min = int(startSeconds / 60)
+      seconds = int(startSeconds % 60)
+      line = str(min) + ":" + str(seconds) + " sample1"
+      f.write(line)
 
-    if os.path.exists(src):
-        labels = data['positive_labels'].split(',')
-        
-        for label in labels:
-            found = ontologyFindByID(label)
+      f.close()
 
-            folder = resultFolder + found['name']
-            dst = folder + "/" + data['YTID'] + ".mp3"
+      call(["python", "split.py", "-yt", url])
 
-            # create destination folder
-            if not os.path.exists(folder):
-                os.makedirs(folder)
-            
-            copyfile(src, dst)
+      if os.path.exists(src):
+          labelIds = data['positive_labels'].split(',')
+          
+          for labelId in labelIds:
+              found = ontologyFindById(labelId)
+
+              folder = resultFolder + found['name']
+              dst = folder + "/" + data['YTID'] + ".mp3"
+
+              # create destination folder
+              if not os.path.exists(folder):
+                  os.makedirs(folder)
+              
+              copyfile(src, dst)
+
+if __name__ == "__main__":
+    # arg parsing
+    parser = argparse.ArgumentParser(description='Download audioes from AudioSet')
+    parser.add_argument(
+        "-id", 
+        help="The id you want to download. Default: None", 
+        metavar="label_id",
+        default=None
+    )
+    parser.add_argument(
+        "-label",
+        help="Specify the label that you want to download. Default: None",
+        metavar="label",
+        default=None
+    )
+
+    args = parser.parse_args()
+    LABELID = args.id
+    LABEL = args.label
+
+    if LABELID is not None:
+      if ontologyFindById(LABELID) is None:
+        print("Error> No Matching ID !")
+      else:
+        download('id', LABELID)
+    elif LABEL is not None:
+      if ontologyFindByLabel(LABEL) is None:
+        print("Error> No Matching Label !")
+      else:
+        download('label', LABEL)
+    else:
+      download(None, None)
